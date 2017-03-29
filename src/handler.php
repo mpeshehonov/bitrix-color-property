79077532dc4920d5ab5570be389434a48501235d
Пешехонов Максим <m.peshehonov@maximaster.ru>
2017-03-29 15:59:28 +0300

    правки путей в установщике

--- src/handler.php
+++ src/handler.php
@@ -6,11 +6,11 @@ if(PHP_SAPI == 'cli') {
 else {
     AddEventHandler('iblock', 'OnIBlockPropertyBuildList', ['ColorProperty', 'getDescription']);
     AddEventHandler('main', 'OnUserTypeBuildList', ['ColorProperty', 'getDescription']);
-    if(file_exists(DOCUMENT_ROOT . '/bitrix/js/jscolor.ForBitrix')) {
+    if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/bitrix/js/jscolor.ForBitrix')) {
         return;
     } else {
         mkdir('/bitrix/js/jscolor.ForBitrix');
-        copy(__DIR__ . '/../install/jscolor.min.js', DOCUMENT_ROOT . '/bitrix/js/jscolor.ForBitrix/jscolor.min.js');
-        copy(__DIR__ . '/../install/jscolor_events.js', DOCUMENT_ROOT . '/bitrix/js/jscolor.ForBitrix/jscolor_events.js');
+        copy(__DIR__ . '/../install/jscolor.min.js', $_SERVER['DOCUMENT_ROOT'] . '/bitrix/js/jscolor.ForBitrix/jscolor.min.js');
+        copy(__DIR__ . '/../install/jscolor_events.js', $_SERVER['DOCUMENT_ROOT'] . '/bitrix/js/jscolor.ForBitrix/jscolor_events.js');
     }
 }
