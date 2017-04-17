6d9d0091b795cb30130f8139f2adcd529777e8a4
Пешехонов Максим <m.peshehonov@maximaster.ru>
2017-04-17 17:40:58 +0300

    мелкие доработки

--- tools/handler.php
+++ tools/handler.php
@@ -6,9 +6,7 @@ if(PHP_SAPI == 'cli') {
 else {
     AddEventHandler('iblock', 'OnIBlockPropertyBuildList', ['\\Peshek\\Properties\\ColorProperty', 'getDescription']);
     AddEventHandler('main', 'OnUserTypeBuildList', ['\\Peshek\\Properties\\ColorProperty', 'getDescription']);
-    if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/bitrix/js/jscolor.ForBitrix')) {
-        return;
-    } else {
+    if(!file_exists($_SERVER['DOCUMENT_ROOT'] . '/bitrix/js/jscolor.ForBitrix')) {
         mkdir($_SERVER['DOCUMENT_ROOT'] . '/bitrix/js/jscolor.ForBitrix');
         copy(__DIR__ . '/../install/jscolor.min.js', $_SERVER['DOCUMENT_ROOT'] . '/bitrix/js/jscolor.ForBitrix/jscolor.min.js');
         copy(__DIR__ . '/../install/jscolor_events.js', $_SERVER['DOCUMENT_ROOT'] . '/bitrix/js/jscolor.ForBitrix/jscolor_events.js');
