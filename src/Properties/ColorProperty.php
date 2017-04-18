32cd7cb47b55f20da1906d548c57d6761534431e
Пешехонов Максим <m.peshehonov@maximaster.ru>
2017-04-18 13:44:17 +0300

    мелкие правки

--- src/Properties/ColorProperty.php
+++ src/Properties/ColorProperty.php
@@ -9,7 +9,7 @@ Loc::loadMessages(__FILE__);
 /**
  * Класс, который отвечает за отображение кастомного свойства типа "Цвет"
  */
-class ColorProperty extends CUserTypeString
+class ColorProperty
 {
     /**
      * Получение описания пользовательского свойства
